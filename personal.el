e5f49a829101631fc38a28540da3582bf0057860
ts <ts@mobvoi.com>
2020-06-04 20:24:22 +0800

    feature: open file from xclipboard directly

--- personal.el
+++ personal.el
@@ -2,6 +2,14 @@
 
 ;; Some of my personal config
 
+;; open file from xclipboard
+;; when I use command 'ff' find a file and copy it's full path to xclipboard, then I can use this function open the file directly in emacs.
+(defun my-open-file-from-clipboard ()
+  (interactive)
+  (let* ((name (shell-command-to-string "xclip -selection c -o")))
+    (if (file-exists-p name)
+        (find-file name))))
+
 (setq my-favourite-color-themes
       '(doom-Iosvkem
         doom-city-lights
@@ -34,4 +42,3 @@
         molokai))
 
 (my-random-color-theme)
-
